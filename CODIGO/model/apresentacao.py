4d2e5627a1f30dbe9280db376f594b57c363e2cd
SAMUEL™ <107000099+VILHALVA@users.noreply.github.com>
2024-09-27 13:31:40 -0500

    Update apresentacao.py

--- CODIGO/model/apresentacao.py
+++ CODIGO/model/apresentacao.py
@@ -3,15 +3,15 @@ from config import *
 def apresentacao():
     cont = 1
     def tempo(txt1, segundos, txt2):
-        print(f"😠Foram {cont} tentativas!!!", end="\r")
+        print(f"😠FORAM {cont} TENTATIVAS!!!", end="\r")
         sleep(2)
         for c in range(segundos, 0, -1):
             print(f"{txt1}: {c} {txt2}", end="\r")
             sleep(1)
-    senha = STRING("😎Digite a senha para começar:\n>>>")
+    senha = STRING("😎DIGITE A SENHA PARA COMEÇAR:\n>>>")
     while senha not in "VILHALVA":
         cont += 1
-        senha = STRING("😎Senha inválida!!! Tente novamente:\n>>>")
+        senha = STRING("😎SENHA INVÁLIDA!!! TENTE NOVAMENTE:\n>>>")
         if cont == 3:
             tempo("⌛AGUARDE", 60, "SEGUNDOS...")
         elif cont == 6:
@@ -21,11 +21,11 @@ def apresentacao():
     print("=" * 35, f"\n👏PARABÉNS!!!\n🔓VOCÊ ACERTOU!!!\n⭐Foram {cont} tentativas!!!\n", "=" * 35)
     sleep(2)
     PROCESSO("⌛INICIANDO", 00, 110, 10, 0.7)
-    name = STRING("👽Para começarmos, por favor, digite o seu nome:\n>>>")
-    print(f"🌞Seja bem vindo ao Gnoses, {name}. Esse programa foi criado para resolver pequenos problemas!")
+    name = STRING("👽PARA COMEÇARMOS, POR FAVOR, DIGITE O SEU NOME:\n>>>")
+    print(f"🌞SEJA BEM VINDO AO GNOSES, {name}. ESSE BOT FOI CRIADO PARA RESOLVER PEQUENOS PROBLEMAS!")
     sleep(1)
     print("😎ESSE BOT FOI CRIADO PELO SAMUEL MARTINS VILHALVA -> 🐱GITHUB: @VILHALVA")
     sleep(3)
-    print("⏬Você irá receber o menu de opções logo abaixo. Basta digitar o número da função que você deseja usar:")
+    print("⏬VOCÊ IRÁ RECEBER O MENU DE OPÇÕES LOGO ABAIXO. BASTA DIGITAR O NÚMERO DA FUNÇÃO QUE VOCÊ DESEJA USAR:")
 
 
